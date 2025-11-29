21758886865a0e5bc7e3e9749b0a1c78c5add706
bebokaka99 <nq20k2@gmail.com>
2025-11-30 00:22:49 +0700

    SEO

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import './index.css'
-
-// --- THÊM DÒNG IMPORT NÀY ---
-import { HelmetProvider } from 'react-helmet-async'; 
+// 1. IMPORT NÀY
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* --- THÊM HELMETPROVIDER WRAPPER --- */}
+    {/* 2. PHẢI CÓ BỌC NGOÀI CÙNG NÀY */}
     <HelmetProvider>
         <RouterProvider router={router} />
     </HelmetProvider>
